f3a63c1979126d1ee907e5c066626a5c9fca1219
OrlandoSte <163282275+OrlandoSte@users.noreply.github.com>
2024-06-03 21:32:45 +0200

    a

--- APP/Programacion1/src/Inscribirse.java
+++ APP/Programacion1/src/Inscribirse.java
@@ -51,19 +51,6 @@ public class Inscribirse extends JFrame {
 		this.dispose();
 	}
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Inscribirse frame = new Inscribirse();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
 	/**
 	 * Create the frame.
 	 */
